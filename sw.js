049a05119d027be3dcb31e56de9945e2dd4ee2bc
Vincent <vincentplym07@gmail.com>
2026-09-23 18:11:41 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,1 +0,0 @@
-const CACHE="a-plus-core2-v1";const ASSETS=["./","./index.html","./styles.css","./app.js","./questions.json","./manifest.webmanifest","./icon.svg"];self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))))
